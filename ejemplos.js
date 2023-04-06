d3b2037bb486bdce1eb15a3a5f8a0610a02226d1
Diego Branco <dbranco86@hotmail.com>
2023-04-05 20:02:14 -0500

    actualizando ejemplos de javascript

--- ejemplos.js
+++ ejemplos.js
@@ -17,11 +17,45 @@
    // alert("Tu nombre de usuario es " + nombreUsuario);
 //}
 
-let edad = Number(prompt("Ingrese su edad "));
-const ESMAYOR = (edad >= 18);
+//let edad = Number(prompt("Ingrese su edad "));
+//const ESMAYOR = (edad >= 18);
 
-if (ESMAYOR){
-    alert("Usted puede ingresar al sitio web");
-}else {
-    alert("Usted no puede ingresar al sitio porque es menor de edad");
+//if (ESMAYOR){
+//    alert("Usted puede ingresar al sitio web");
+//}else {
+//    alert("Usted no puede ingresar al sitio porque es menor de edad");
+//}
+
+//let nombreUsuario= prompt("Ingrese su nombre");
+//let apellidoUsuario= prompt("Ingrese su apellido");
+
+//if ((nombreUsuario != "") && (apellidoUsuario != "")){
+//    alert("Bienvenido " + nombreUsuario + " " + apellidoUsuario);
+//}else {
+ //   alert("ERROR: Falta datos !");
+//}
+
+//let numero= Number (prompt("ingrese un número"));
+
+//if (numero < 1000) {
+//    alert ("Correcto, el número ingresado es " + numero);
+//}else{
+   // alert("Incorrecto, el número es mayor a 1000")
+//}
+
+//for (let i=0; i<=4; i++){
+    //console.log("Interación n°= " + i);
+   // document.write ("Interación n°= " + i);
+//}
+
+//for(let i=1; i<=5; i++){
+   // let ingresarNombre= prompt("Ingrese nombre ");
+   // alert("Turno N°" + " " + i +  " " + "Nombre:" + ingresarNombre);
+//}
+
+let entrada = prompt ("Ingrese un dato: ");
+
+while (entrada != "ESC"){
+    alert ("El usuario ingresó: " + entrada);
+    entrada = prompt ("Ingrese otro dato: ");
 }
