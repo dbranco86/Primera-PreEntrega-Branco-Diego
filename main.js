d36096fb9593ae8aca8d6747557a1eca12bdd482
Diego Branco <dbranco86@hotmail.com>
2023-04-16 01:11:02 -0500

    Trabajando primera pre-entrega

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ const PRODUCTO_3 = "MILANESA";
 const PRODUCTO_4 = "REFRESCO"
 const TIPO_MONEDA = "USD"
 
-alert("Bienvenidos a Fast Food \n" + "INSUMOS DEL DIA: \n\n" + PRODUCTO_1 + " " + "10" + TIPO_MONEDA + " " + "\n" + PRODUCTO_2 + " " + "15" + TIPO_MONEDA + " " + "\n" + PRODUCTO_3 + " " + "20" + TIPO_MONEDA + " " + "\n" + PRODUCTO_4 + " " + "5" + TIPO_MONEDA);
-let seleccion = prompt ("Bienvenidos a Fast Food \n" + "Seleccione un producto de la lista: \n\n" + PRODUCTO_1 + "\n" + PRODUCTO_2 + "\n" + PRODUCTO_3 + "\n" + PRODUCTO_4);
+alert("Bienvenidos a Fast Food \n" + "LISTA DE INSUMOS Y PRECIOS: \n\n" + PRODUCTO_1 + " " + "10" + TIPO_MONEDA + " " + "\n" + PRODUCTO_2 + " " + "15" + TIPO_MONEDA + " " + "\n" + PRODUCTO_3 + " " + "20" + TIPO_MONEDA + " " + "\n" + PRODUCTO_4 + " " + "5" + TIPO_MONEDA);
+let seleccion = prompt ("Seleccione un producto de la lista: \n\n" + PRODUCTO_1 + "\n" + PRODUCTO_2 + "\n" + PRODUCTO_3 + "\n" + PRODUCTO_4);
 let precioProducto;
 let confirmar;
 let cantidad;
+let precioTotal;
 
 // COMPRUEBA  EL INGRESO DE DATOS //
 while (seleccion == ""){
@@ -23,19 +24,20 @@ while ((seleccion != PRODUCTO_1) && (seleccion != PRODUCTO_2) && (seleccion != P
    seleccion = prompt ("Seleccione un producto de la lista: \n\n" + PRODUCTO_1 + "\n" + PRODUCTO_2 + "\n" + PRODUCTO_3 + "\n" + PRODUCTO_4);
 }
 
-   if (seleccion == PRODUCTO_1){
-      precioProducto = 10;
-      alert("Usted ha escogido EMPANADA");
-   }else if (seleccion == PRODUCTO_2){
-      precioProducto = 15;
-      alert("Usted ha escogido HAMBURGUESA");
-   }else if (seleccion == PRODUCTO_3){
-      precioProducto = 20;
-      alert("Usted ha escogido MILANESA");
-   }else if (seleccion == PRODUCTO_4){
-      precioProducto = 5;
-      alert("Usted ha escogido Refresco");
-   }
+// EVALUA LA CONDICION //
+if (seleccion == PRODUCTO_1){
+   mensaje();
+   precioProducto = 10;
+}else if (seleccion == PRODUCTO_2){
+   mensaje();
+   precioProducto = 15;
+}else if (seleccion == PRODUCTO_3){
+   mensaje();
+   precioProducto = 20;
+}else if (seleccion == PRODUCTO_4){
+   mensaje();
+   precioProducto = 5;
+}
 
 // PIDE LA CANTIDAD //
 do {
@@ -60,15 +62,22 @@ while ((confirmar.toUpperCase() != "S") && (confirmar.toUpperCase() != "N")) {
 
 // CONFIRMAR COMPRA //
 if (confirmar.toUpperCase() == "S"){
-   let precioTotal = cantidad * precioProducto;
+   //precioTotal = cantidad * precioProducto;
+   precioFinal(cantidad, precioProducto);
    alert ("Su orden es: \n" + "\n" + "Producto: " + seleccion + "\n" + "Cantidad: " + cantidad + "\n" + "Total a pagar: " + precioTotal + TIPO_MONEDA);
    alert ("Gracias por su compra!");
 }else if (confirmar.toUpperCase() == "N") {
    alert ("Gracias por su visita!");
 }
 
+// FUNCIONES //
+function mensaje(){
+   alert ("Usted ha escogido" + " " + seleccion);
+}
 
-
+function precioFinal (cantidad, precioProducto){
+   precioTotal = cantidad * precioProducto;
+}
 
 
 
